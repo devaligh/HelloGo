000a6b2c3c4b12fb1b2fbb590af607a5ee8c18f9
digimaktab <alighaffari70@gmail.com>
2021-11-04 10:19:50 +0330

    Add emoji

--- S01E01-HelloWorld.go
+++ S01E01-HelloWorld.go
@@ -1,11 +1,18 @@
-// in the name of Allah
+// 🕋 in the name of God
+// 📺 S01E01 - Hello World by Go language
+// 📃 https://github.com/devaligh
+// 👨 https://tiwtter.com/ali_ghaffari
 
+// 📦 set package name
 package main
 
+// 📚 import fmt library for use Print in screen function
 import (
 	"fmt"
 )
 
+// 💣 main function is startup finction by default
 func main() {
-	fmt.Println("HelloWorld")
+	// show Hello World in screen
+	fmt.Println("HelloWorld 🌎")
 }
